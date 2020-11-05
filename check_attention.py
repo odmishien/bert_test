810f2edbe38db64677bb73942dab6203176261ee
odmishien <tmtm.yagi.007@gmail.com>
2020-11-05 05:18:52 +0000

    なんかmk_highが2つあった

--- check_attention.py
+++ check_attention.py
@@ -7,13 +7,9 @@ import dataset_jp_text as ds_jptxt
 import dataset_IMDb as ds_imdb
 from bert_cls import BertClassifier
 
-<<<<<<< HEAD
-import os, csv
-=======
 import csv
 import os
 
->>>>>>> 2e25875035568f50964b9041b6fc2f473e37ef74
 
 def parse_arg():
     parser = argparse.ArgumentParser(description="Predict using BERT model.")
@@ -161,52 +157,6 @@ def mk_high_attention_words_list(sentence, label, pred, normlized_weights, token
     with open(filename, 'w') as f:
         writer = csv.writer(f, delimiter='\t')
         writer.writerows(a)
-
-def mk_high_attention_words_list(index, batch, preds, normlized_weights, tokenizer, tsv_path):
-     # indexの結果を抽出
-    sentence = batch.Text[0][index]  # 文章
-    label = batch.Label[index]  # ラベル
-    pred = preds[index]  # 予測
-
-    # ラベルと予測結果を文字に置き換え
-    if label == 0:
-        label_str = "Negative"
-    else:
-        label_str = "Positive"
-
-    if pred == 0:
-        pred_str = "Negative"
-    else:
-        pred_str = "Positive"
-
-    # Self-Attentionの重みを可視化。Multi-Headが12個なので、12種類のアテンションが存在
-    for i in range(12):
-        a = []
-
-        # indexのAttentionを抽出と規格化
-        # 0単語目[CLS]の、i番目のMulti-Head Attentionを取り出す
-        # indexはミニバッチの何個目のデータかをしめす
-        attens = normlized_weights[index, i, 0, :]
-        attens /= attens.max()
-
-        for word, attn in zip(sentence, attens):
-            # 単語が[SEP]の場合は文章が終わりなのでbreak
-            if tokenizer.convert_ids_to_tokens([word.numpy().tolist()])[0] == "[SEP]":
-                break
-            a.append([attn.cpu().detach().clone().numpy(
-            ), tokenizer.convert_ids_to_tokens([word.numpy().tolist()])[0]])
-        filename = f'{tsv_path}/attn_{i}.tsv'
-
-        if not os.path.exists(os.path.dirname(filename)):
-            try:
-                os.makedirs(os.path.dirname(filename))
-            except OSError as exc:  # Guard against race condition
-                if exc.errno != errno.EEXIST:
-                    raise
-        with open(filename, 'w') as f:
-            writer = csv.writer(f, delimiter='\t')
-            writer.writerows(a)
-
 
 def predict(net, inputs):
     outputs, attention_probs = net(

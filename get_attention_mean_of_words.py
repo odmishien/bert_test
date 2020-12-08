9cf055048847678517a417b25d1a9a76bfb014cf
odmishien <tmtm.yagi.007@gmail.com>
2020-12-08 17:03:29 +0900

    fontSize:18

--- get_attention_mean_of_words.py
+++ get_attention_mean_of_words.py
@@ -28,7 +28,7 @@ def get_target_files(target_dir, target_attn_layer, target_lp):
 
 
 def plot(df, title, path):
-    seaborn.set(rc={'figure.figsize': (200, 100)})
+    seaborn.set(rc={"font.size": 18, 'figure.figsize': (200, 100)})
     seaborn.barplot(data=df, x='word', y='attn_mean',
                     order=df.sort_values('attn_mean', ascending=False)['word'][:50]).set_title(title)
     plt.xticks(rotation=90)
